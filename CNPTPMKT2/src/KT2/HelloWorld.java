9bd97981db26c5324f38a768e0523c9920cb3c32
thangdv007 <thangdv007@gmail.com>
2023-06-01 10:54:55 +0700

    merge

--- CNPTPMKT2/src/KT2/HelloWorld.java
+++ CNPTPMKT2/src/KT2/HelloWorld.java
@@ -3,5 +3,6 @@ package KK2;
 public class HelloWorld {
 	public static void main(String[] args) {
 	    System.out.println("Hello World!");
+		System.out.println("Nguyễn Kim Thăng - 2019605567 - KT2");
 	  }
 }
